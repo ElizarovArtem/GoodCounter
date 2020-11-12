d666e5511da2d81f50cf916463aefa2c0cba6bd1
ElizarovArtem <elizarovskiyklach@yandex.ru>
2020-11-12 20:39:00 +0300

    Updates

---
+++ precache-manifest.9e53ee4905095ef98b067be0b85e05f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078fc9557785fbc8fae2bfae313d5ec5",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "52c2524cb58b20481f48",
+    "url": "/Counter/static/css/main.6c8a6d6c.chunk.css"
+  },
+  {
+    "revision": "e35775ac677059596903",
+    "url": "/Counter/static/js/2.2643dc57.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Counter/static/js/2.2643dc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c2524cb58b20481f48",
+    "url": "/Counter/static/js/main.234abb1e.chunk.js"
+  },
+  {
+    "revision": "4a2bea4ebda10b11d1d6",
+    "url": "/Counter/static/js/runtime-main.a4bce0b8.js"
+  }
+]);
